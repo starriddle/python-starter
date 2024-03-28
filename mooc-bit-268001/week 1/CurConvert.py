695a06c700f7766a73ae3e70816e025ccf26b825
starriddle <starriddle@outlook.com>
2024-03-28 13:52:04 +0800

    代码规范化

--- mooc-bit-268001/week 1/CurConvert.py
+++ mooc-bit-268001/week 1/CurConvert.py
@@ -1,11 +1,22 @@
 #!/usr/bin/python3
 
+"""
+货币转换
+
+人民币和美元是世界上通用的两种货币之一，写一个程序进行货币间币值转换，其中：
+人民币和美元间汇率固定为：1美元 = 6.78人民币。
+程序可以接受人民币或美元输入，转换为美元或人民币输出。人民币采用RMB表示，美元USD表示，符号和数值之间没有空格。
+
+注意：
+(1) 这是一个OJ题目，获得输入请使用input() ；
+"""
+
 CurStr = input("请输入带有符号的金额：")
-if CurStr[0:3] == "RMB" :
+if CurStr[0:3] == "RMB":
     USD = eval(CurStr[3:]) / 6.78
     print("转换后的金额是：USD{:.2f}".format(USD))
-elif CurStr[0:3] == "USD" :
+elif CurStr[0:3] == "USD":
     RMB = eval(CurStr[3:]) * 6.78
     print("转换后的金额是：RMB{:.2f}".format(RMB))
-else :
+else:
     print("输入格式错误")
--- mooc-bit-268001/week 1/NumConvert.py
+++ mooc-bit-268001/week 1/NumConvert.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
 
+"""
+数字形式转换
+
+获得用户输入的一个正整数输入，输出该数字对应的中文字符表示。
+0到9对应的中文字符分别是：零一二三四五六七八九
+"""
+
 number = '零一二三四五六七八九'
 s = input("请输入一个正整数：")
-for c in s :
+for c in s:
     print(number[eval(c)], end="")
 print()
