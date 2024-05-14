abb6be4b0f890c4a4759fb3037da0e0405ca2bfb
starriddle <starriddle@outlook.com>
2024-05-14 18:07:38 +0800

    Stack Management System init

---
+++ shou-ai/StockManagementSystem.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+
+"""
+库存管理系统
+
+使用面向对象的方法设计一个库存管理系统，具备以下功能：
+商品入库
+--输入商品名称，价格，数量，序列号入库。
+商品出库
+--输入商品名称和数量出库，处理商品重名情况。
+--输入商品序列化和数量出库。
+商品查询
+--输入商品名称查询。
+--输入商品序列号查询。
+商品列表 库存统计
+--统计商品类型数量，商品总数量，商品总价值。
+--通过默认最小库存参数显示库存不足商品。
+--通过设置最小库存参数显示库存不足商品。
+输入输出
+--将商品列表导出至文件。
+--从文件导入商品列表。
+--将商品列表导出至EXCEL。
+--从EXCEL导入商品列表。
+
+第三方库：
+- pandas: 可以读取各种各样格式的数据文件，支持 txt、csv、excel、json、剪切板、数据库、html、hdf、parquet、pickled文件、sas、stata等等
+- xlrd: 从 excel 中读取数据，支持 xls 和 xlsx
+- xlwt: 对 excel 进行修改操作，支持 xls，不支持 xlsx
+- xlutils: 在 xlwt 和 xlrd 中，对一个已存在的文件进行修改
+- xlsxwriter: 生成 excel 表格，插入数据、插入图表等表格操作，不支持读取
+- openpyxl: 对 xlsx 文件进行读取和编辑
+- xlwings: 对 xls 、xlsx、xlsm 文件进行读写、格式修改等操作
+"""
+
+
+class Product(object):
+    """
+
+    """
+    def __init__(self):
+        """
+
+        """
+
+
+class Stock(object):
+    """
+
+    """
+    def __init__(self):
+        """
+
+        """
+
+
+class StockIO(object):
+    """
+
+    """
+    def __init__(self):
+        """
+
+        """
