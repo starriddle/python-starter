55825e56a19c7e09c49f4938883eeb53d0df4ee3
starriddle <starriddle@outlook.com>
2024-05-15 17:44:58 +0800

    Sentiment Analysis update comment

--- shou-ai/SentimentAnalysis.py
+++ shou-ai/SentimentAnalysis.py
@@ -15,6 +15,9 @@ def analysis(file_path, print_analysis=False, print_result=False, print_final=Tr
     """
     读取数据文件，使用 SNOWNLP 库进行分析，输出结果
     :param file_path: 数据文件路径
+    :param print_analysis: 分析时是否输出原始顺序的每个句子的情感分值
+    :param print_result: 分析完毕后是否输出按分值排序的每个句子的情感分值
+    :param print_final: 最后是否输出所有句子的平均情感分值（排除最高/最低分值）
     :return:
     """
     f = open(file_path, "r", encoding="UTF-8")
